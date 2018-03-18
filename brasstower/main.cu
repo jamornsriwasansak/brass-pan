47408ed5360896919b9d7b2ed94294fd8efcfe0b
Jamorn Sriwasansak@home <zentojamorn@gmail.com>
2018-03-18 20:02:19 +0900

    clean main.cu and add fps counter

--- brasstower/main.cu
+++ brasstower/main.cu
@@ -9,6 +9,9 @@
 #include <device_launch_parameters.h>
 #include <cuda_gl_interop.h>
 
+#include <ctime>
+#include <chrono>
+
 #include "ext/helper_math.h"
 #include "opengl/shader.h"
 #include "mesh.h"
@@ -16,46 +19,6 @@
 
 #include "particlerenderer.cuh"
 #include "particlesolver.cuh"
-
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-	int i = threadIdx.x;
-	c[i] = a[i] + b[i];
-}
-
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-	int *dev_a = 0;
-	int *dev_b = 0;
-	int *dev_c = 0;
-	cudaError_t cudaStatus;
-
-	// Choose which GPU to run on, change this on a multi-GPU system.
-	cudaStatus = cudaSetDevice(0);
-	cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-	cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-	// Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-	cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-	// Launch a kernel on the GPU with one thread for each element.
-	addKernel<<<1, size >> > (dev_c, dev_a, dev_b);
-
-	// Check for any errors launching the kernel
-	cudaStatus = cudaGetLastError();
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
-	cudaStatus = cudaDeviceSynchronize();
-	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-
-Error:
-	cudaFree(dev_c);
-	cudaFree(dev_a);
-	cudaFree(dev_b);
-
-	return cudaStatus;
-}
 
 GLFWwindow * window;
 ParticleRenderer * renderer;
@@ -132,17 +95,16 @@ __global__ void mapPositions(float4 * ssboDptr, float3 * position)
 int main()
 {
 	cudaGLSetGLDevice(0);
-	int numParticles = 10;
-
-	float p = 0.0f;
+	window = InitGL(1280, 720);
 
 	std::shared_ptr<Scene> scene = initSimpleScene();
-
-	window = InitGL(1280, 720);
 	renderer = new ParticleRenderer(glm::uvec2(1280, 720), scene);
 	solver = new ParticleSolver(scene);
-
 	solver->addParticles(glm::ivec3(4), glm::vec3(0, 1, 0), glm::vec3(0.01f * 2.0f), 1.0f);
+
+	// fps counter
+	std::chrono::high_resolution_clock::time_point lastUpdateTime = std::chrono::high_resolution_clock::now();
+	int numFrame = 0;
 
 	do
 	{
@@ -161,6 +123,17 @@ int main()
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
+		// update fps counter
+		std::chrono::high_resolution_clock::time_point currentTime = std::chrono::high_resolution_clock::now();
+		long long elapsedMs = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastUpdateTime).count();
+		if (elapsedMs >= 100)
+		{
+			glfwSetWindowTitle(window, std::to_string((float)elapsedMs / (float)numFrame).c_str());
+			numFrame = 0;
+			lastUpdateTime = currentTime;
+		}
+
+		numFrame++;
 	} // Check if the ESC key was pressed or the window was closed
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
