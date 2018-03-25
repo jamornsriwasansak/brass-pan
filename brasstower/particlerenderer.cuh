d227c2423b17ab19d66321336a95748a876d4e28
Jamorn Sriwasansak@home <zentojamorn@gmail.com>
2018-03-25 12:58:02 +0900

    increase to 16 samples aa

--- brasstower/particlerenderer.cuh
+++ brasstower/particlerenderer.cuh
@@ -76,7 +76,7 @@ static GLFWwindow* InitGL(const size_t width, const size_t height)
 		throw new std::exception("Failed to initialize GLFW\n");
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 4); // antialiasing
+	glfwWindowHint(GLFW_SAMPLES, 16); // antialiasing
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
