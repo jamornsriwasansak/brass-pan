cca3b5b2235075a6d3b2ffc05b40a8962eedda8f
Jamorn Sriwasansak <zentojamorn@gmail.com>
2018-05-12 21:01:43 +0900

    optimize kernel evaluation (a bit faster)

--- brasstower/kernel/fluid.cuh
+++ brasstower/kernel/fluid.cuh
@@ -27,11 +27,15 @@ void SetKernelRadius(float h)
 	checkCudaErrors(cudaMemcpyToSymbol(KernelRadius, &const7, sizeof(float)));
 }
 
+//#define USE_KERNEL_DISTANCE_CHECK
+
 __device__
 const float poly6Kernel(const float r2)
 {
 	/// TODO:: precompute these
+#ifdef USE_KERNEL_DISTANCE_CHECK
 	if (r2 <= KernelSquaredRadius)
+#endif
 	{
 		const float temp = KernelSquaredRadius - r2;
 		return KernelConst1 * temp * temp * temp;
@@ -42,7 +46,9 @@ const float poly6Kernel(const float r2)
 __device__
 const float3 gradientSpikyKernel(const float3 v, const float r2)
 {
+#ifdef USE_KERNEL_DISTANCE_CHECK
 	if (r2 <= KernelSquaredRadius && r2 > 0.f)
+#endif
 	{
 		const float r = sqrtf(r2);
 		const float temp = KernelRadius - r;
@@ -54,7 +60,9 @@ const float3 gradientSpikyKernel(const float3 v, const float r2)
 __device__
 const float akinciSplineC(const float r) // akinci used 2*r instead of r
 {
+#ifdef USE_KERNEL_DISTANCE_CHECK
 	if (r < KernelRadius && r > 0)
+#endif
 	{
 		const float temp = (KernelRadius - r) * r;
 		const float temp3 = temp * temp * temp;
@@ -99,36 +107,38 @@ fluidLambda(float * __restrict__ lambdas,
 #endif
 
 	const int3 centerGridPos = calcGridPos(newPositionsPrev[i], cellOrigin, cellSize);
-	const int3 start = centerGridPos - gridSearchOffset;
-	const int3 end = centerGridPos + gridSearchOffset;
+	const int3 searchStart = centerGridPos - gridSearchOffset;
+	const int3 searchEnd = centerGridPos + gridSearchOffset;
 
-	for (int z = start.z; z <= end.z; z++)
-		for (int y = start.y; y <= end.y; y++)
-			for (int x = start.x; x <= end.x; x++)
+	for (int z = searchStart.z; z <= searchEnd.z; z++)
+		for (int y = searchStart.y; y <= searchEnd.y; y++)
+			for (int x = searchStart.x; x <= searchEnd.x; x++)
 			{
 				const int3 gridPos = make_int3(x, y, z);
 				const int gridAddress = calcGridAddress(gridPos, gridSize);
-				int start = cellStart[gridAddress];
-				int end = cellEnd[gridAddress];
-				for (int j = start; j < end; j++)
+				int neighbourStart = cellStart[gridAddress];
+				int neighbourEnd = cellEnd[gridAddress];
+				for (int j = neighbourStart; j < neighbourEnd; j++)
 				{
 					if (i <= j)
 					{
-						const float massj = masses[j];
 						const float3 pj = newPositionsPrev[j];
 						const float3 diff = pi - pj;
 						const float dist2 = length2(pi - pj);
+						if (dist2 <= KernelSquaredRadius)
+						{
+							// density
+							const float massj = masses[j];
+							const float liquidDensity = massj * poly6Kernel(dist2);
+							density += (phases[j] < 0) ? liquidDensity : solidDensity * liquidDensity;
 
-						// density
-						const float liquidDensity = massj * poly6Kernel(dist2);
-						density += (phases[j] < 0) ? liquidDensity : solidDensity * liquidDensity;
-
-					#ifndef USE_PRECOMPUTED_FLUID
-						// gradient for lambda
-						const float3 gradient = -massj * gradientSpikyKernel(diff, dist2) / restDensity;
-						sumGradient2 += dot(gradient, gradient);
-						gradientI -= gradient;
-					#endif
+						#ifndef USE_PRECOMPUTED_FLUID
+							// gradient for lambda
+							const float3 gradient = -massj * gradientSpikyKernel(diff, dist2) / restDensity;
+							sumGradient2 += dot(gradient, gradient);
+							gradientI -= gradient;
+						#endif
+						}
 					}
 				}
 			}
@@ -176,37 +186,40 @@ fluidPosition(float3 * __restrict__ deltaXs,
 
 	float3 sum = make_float3(0.f);
 	const int3 centerGridPos = calcGridPos(newPositionsPrev[i], cellOrigin, cellSize);
-	const int3 start = centerGridPos - gridSearchOffset;
-	const int3 end = centerGridPos + gridSearchOffset;
+	const int3 searchStart = centerGridPos - gridSearchOffset;
+	const int3 searchEnd = centerGridPos + gridSearchOffset;
 
-	for (int z = start.z; z <= end.z; z++)
-		for (int y = start.y; y <= end.y; y++)
-			for (int x = start.x; x <= end.x; x++)
+	for (int z = searchStart.z; z <= searchEnd.z; z++)
+		for (int y = searchStart.y; y <= searchEnd.y; y++)
+			for (int x = searchStart.x; x <= searchEnd.x; x++)
 			{
 				const int3 gridPos = make_int3(x, y, z);
 				const int gridAddress = calcGridAddress(gridPos, gridSize);
-				int start = cellStart[gridAddress];
-				int end = cellEnd[gridAddress];
-				for (int j = start; j < end; j++)
+				int neighbourStart = cellStart[gridAddress];
+				int neighbourEnd = cellEnd[gridAddress];
+				for (int j = neighbourStart; j < neighbourEnd; j++)
 				{
 					if (i != j && phases[j] < 0)
 					{
-						const float massj = masses[j];
 						const float3 pj = newPositionsPrev[j];
-						float sumLambda = lambdas[i] + lambdas[j];
 						const float3 diff = pi - pj;
 						const float dist2 = length2(diff);
-						if (!useAkinciCohesionTension) sumLambda += -K * powf(poly6Kernel(dist2) / poly6Kernel(powf(0.03f * KernelRadius, 2.f)), N);
-						float3 deltaX = massj * sumLambda * gradientSpikyKernel(pi - pj, dist2);
+						if (dist2 <= KernelSquaredRadius && dist2 > 0)
+						{
+							const float massj = masses[j];
+							float sumLambda = lambdas[i] + lambdas[j];
+							if (!useAkinciCohesionTension) sumLambda += -K * powf(poly6Kernel(dist2) / poly6Kernel(powf(0.03f * KernelRadius, 2.f)), N);
+							float3 deltaX = massj * sumLambda * gradientSpikyKernel(pi - pj, dist2);
 
-						// clamp deltaposition to increase solver stability
-						// good parameters can trade-off realism for performance
-					#if 1
-						float lenMove = length(deltaX);
-						deltaX = (lenMove < 5.0f) ? deltaX : deltaX * min(lenMove, 5.0f) / lenMove;
-					#endif
+							// clamp deltaposition to increase solver stability
+							// good parameters can trade-off realism for performance
+						#if 1
+							float lenMove = length(deltaX);
+							deltaX = (lenMove < 5.0f) ? deltaX : deltaX * min(lenMove, 5.0f) / lenMove;
+						#endif
 
-						sum += deltaX;
+							sum += deltaX;
+						}
 					}
 				}
 			}
@@ -240,23 +253,29 @@ fluidOmega(float3 * __restrict__ omegas,
 
 	float3 omegai = make_float3(0.f);
 	const int3 centerGridPos = calcGridPos(positions[i], cellOrigin, cellSize);
-	const int3 start = centerGridPos - gridSearchOffset;
-	const int3 end = centerGridPos + gridSearchOffset;
+	const int3 searchStart = centerGridPos - gridSearchOffset;
+	const int3 searchEnd = centerGridPos + gridSearchOffset;
 
-	for (int z = start.z; z <= end.z; z++)
-		for (int y = start.y; y <= end.y; y++)
-			for (int x = start.x; x <= end.x; x++)
+	for (int z = searchStart.z; z <= searchEnd.z; z++)
+		for (int y = searchStart.y; y <= searchEnd.y; y++)
+			for (int x = searchStart.x; x <= searchEnd.x; x++)
 			{
 				const int3 gridPos = make_int3(x, y, z);
 				const int gridAddress = calcGridAddress(gridPos, gridSize);
-				for (int j = cellStart[gridAddress]; j < cellEnd[gridAddress]; j++)
+				int neighbourStart = cellStart[gridAddress];
+				int neighbourEnd = cellEnd[gridAddress];
+				for (int j = neighbourStart; j < neighbourEnd; j++)
 				{
 					if (i != j && phasei == phases[j])
 					{
 						const float3 pj = positions[j];
-						const float3 vj = velocities[j];
 						const float3 diff = pi - pj;
-						omegai += cross(vj - vi, gradientSpikyKernel(diff, length2(diff)));
+						const float dist2 = length2(diff);
+						if (dist2 <= KernelSquaredRadius && dist2 > 0)
+						{
+							const float3 vj = velocities[j];
+							omegai += cross(vj - vi, gradientSpikyKernel(diff, dist2));
+						}
 					}
 				}
 			}
@@ -288,24 +307,30 @@ fluidVorticity(float3 * __restrict__ velocities,
 	const float3 omegai = omegas[i];
 	const float3 pi = positions[i];
 	const int3 centerGridPos = calcGridPos(positions[i], cellOrigin, cellSize);
-	const int3 start = centerGridPos - gridSearchOffset;
-	const int3 end = centerGridPos + gridSearchOffset;
+	const int3 searchStart = centerGridPos - gridSearchOffset;
+	const int3 searchEnd = centerGridPos + gridSearchOffset;
 
 	float3 eta = make_float3(0.f);
 
-	for (int z = start.z; z <= end.z; z++)
-		for (int y = start.y; y <= end.y; y++)
-			for (int x = start.x; x <= end.x; x++)
+	for (int z = searchStart.z; z <= searchEnd.z; z++)
+		for (int y = searchStart.y; y <= searchEnd.y; y++)
+			for (int x = searchStart.x; x <= searchEnd.x; x++)
 			{
 				const int3 gridPos = make_int3(x, y, z);
 				const int gridAddress = calcGridAddress(gridPos, gridSize);
-				for (int j = cellStart[gridAddress]; j < cellEnd[gridAddress]; j++)
+				int neighbourStart = cellStart[gridAddress];
+				int neighbourEnd = cellEnd[gridAddress];
+				for (int j = neighbourStart; j < neighbourEnd; j++)
 				{
 					if (i != j && phasei == phases[j])
 					{
 						const float3 pj = positions[j];
 						const float3 diff = pi - pj;
-						eta += length(omegas[j]) * gradientSpikyKernel(diff, length2(diff));
+						const float dist2 = length2(diff);
+						if (dist2 <= KernelSquaredRadius && dist2 > 0)
+						{
+							eta += length(omegas[j]) * gradientSpikyKernel(diff, dist2);
+						}
 					}
 				}
 			}
@@ -341,23 +366,30 @@ fluidXSph(float3 * __restrict__ newVelocities,
 
 	const float3 pi = positions[i];
 	const int3 centerGridPos = calcGridPos(pi, cellOrigin, cellSize);
-	const int3 start = centerGridPos - gridSearchOffset;
-	const int3 end = centerGridPos + gridSearchOffset;
+	const int3 searchStart = centerGridPos - gridSearchOffset;
+	const int3 searchEnd = centerGridPos + gridSearchOffset;
 
 	float3 vnew = make_float3(0.f);
 	const float3 vi = velocities[i];
-	for (int z = start.z; z <= end.z; z++)
-		for (int y = start.y; y <= end.y; y++)
-			for (int x = start.x; x <= end.x; x++)
+	for (int z = searchStart.z; z <= searchEnd.z; z++)
+		for (int y = searchStart.y; y <= searchEnd.y; y++)
+			for (int x = searchStart.x; x <= searchEnd.x; x++)
 			{
 				const int3 gridPos = make_int3(x, y, z);
 				const int gridAddress = calcGridAddress(gridPos, gridSize);
-				for (int j = cellStart[gridAddress]; j < cellEnd[gridAddress]; j++)
+				int neighbourStart = cellStart[gridAddress];
+				int neighbourEnd = cellEnd[gridAddress];
+				for (int j = neighbourStart; j < neighbourEnd; j++)
 				{
 					if (i != j && phasei == phases[j])
 					{
 						float3 pj = positions[j];
-						vnew += (velocities[j] - vi) * poly6Kernel(length2(pi - pj));
+						const float3 diff = pi - pj;
+						const float dist2 = length2(diff);
+						if (dist2 <= KernelSquaredRadius)
+						{
+							vnew += (velocities[j] - vi) * poly6Kernel(dist2);
+						}
 					}
 				}
 			}
@@ -397,13 +429,19 @@ fluidNormal(float3 * __restrict__ normals,
 			{
 				const int3 gridPos = make_int3(x, y, z);
 				const int gridAddress = calcGridAddress(gridPos, gridSize);
-				for (int j = cellStart[gridAddress]; j < cellEnd[gridAddress]; j++)
+				int neighbourStart = cellStart[gridAddress];
+				int neighbourEnd = cellEnd[gridAddress];
+				for (int j = neighbourStart; j < neighbourEnd; j++)
 				{
 					if (i != j && phasei == phases[j])
 					{
 						const float3 pj = positions[j];
 						const float3 diff = pi - pj;
-						normal += 1.0f / densities[j] * gradientSpikyKernel(diff, length2(diff));
+						const float dist2 = length2(diff);
+						if (dist2 <= KernelSquaredRadius && dist2 > 0)
+						{
+							normal += 1.0f / densities[j] * gradientSpikyKernel(diff, dist2);
+						}
 					}
 				}
 			}
@@ -453,15 +491,16 @@ fluidAkinciTension(float3 * __restrict__ newVelocities,
 					if (i != j && phasei == phases[j])
 					{
 						const float3 pj = positions[j];
-						const float lenPiPj = length(pi - pj);
-						if (lenPiPj > 0.f)
+						const float3 diff = pi - pj;
+						const float dist2 = length2(diff);
+						if (dist2 <= KernelSquaredRadius && dist2 > 0.f)
 						{
-							const float3 fCohesion = -surfaceTension * akinciSplineC(lenPiPj) * (pi - pj) / lenPiPj;
+							const float dist = sqrt(dist2);
+							const float3 fCohesion = -surfaceTension * akinciSplineC(dist) * (pi - pj) / dist;
 							const float3 fCurvature = -surfaceTension * (normals[i] - normals[j]);
 							const float kij = 2.0f * restDensity / (densities[i] + densities[j]);
 							fTension += kij * (fCohesion + fCurvature);
 						}
-
 					}
 				}
 			}
